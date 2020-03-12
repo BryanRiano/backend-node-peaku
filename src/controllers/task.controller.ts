7c45603a2e0e46a586cdab29ac8aa9e1f11f1cb0
brayan <brayanriano21@gmail.com>
2020-03-11 21:26:13 -0500

    se agrega cambio a controlador

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -5,10 +5,12 @@ import { Delivery } from '../models/delivery.model';
 
 export function getTasks(req: Request, res: Response) {
     if (req.params) {
-        Task.findAll<Task>({include: [
-            { model: Delivery, as: 'pedido' }
-        ], where: { idconductor: req.params.idconductor, fecha: req.params.fecha } })
-            .then((task: Array<Task>) => res.status(200).json(task))
+        Task.findAll<Task>({
+            include: [
+                { model: Delivery, as: 'pedido' }
+            ], where: { idconductor: req.params.idconductor, fecha: req.params.fecha }
+        })
+            .then((task) => res.status(200).json(task))
             .catch((err: Error) => res.status(500).json(err));
     }
 }
